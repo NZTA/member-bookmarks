13de5c47424c996aac0ccda72468b171d865d3c8
StephenMak <stephen.makrogianni@silverstripe.com>
2019-07-04 15:13:59 +1200

    Add sorting to GlobalBookmarks

--- src/model/GlobalBookmarkModelAdmin.php
+++ src/model/GlobalBookmarkModelAdmin.php
@@ -4,10 +4,11 @@ namespace NZTA\MemberBookmark\Models;
 use Sheadawson\Linkable\Models\Link;
 use SilverStripe\Admin\ModelAdmin;
 use NZTA\MemberBookmark\Models\GlobalBookmark;
+use SilverStripe\Forms\GridField\GridField;
+use UndefinedOffset\SortableGridField\Forms\GridFieldSortableRows;
 
 class GlobalBookmarkModelAdmin extends ModelAdmin
 {
-
     /**
      * @var string
      */
@@ -24,4 +25,22 @@ class GlobalBookmarkModelAdmin extends ModelAdmin
     private static $managed_models = [
         GlobalBookmark::class
     ];
+
+    public function getEditForm($id = null, $fields = null)
+    {
+        $form = parent::getEditForm($id, $fields);
+
+        if ($this->modelClass == GlobalBookmark::class
+            && $gridField = $form->Fields()->dataFieldByName(
+                $this->sanitiseClassName($this->modelClass)
+            )
+        ) {
+            if ($gridField instanceof GridField) {
+                $gridField->getConfig()
+                    ->addComponent(new GridFieldSortableRows('SortOrder'));
+            }
+        }
+
+        return $form;
+    }
 }
